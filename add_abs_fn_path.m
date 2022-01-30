be93508221f3258acc55bde9f2744edc9f46446e
Tomas Kremel <kremelt@gmail.com>
2022-01-30 23:49:26 +0100

    Addpath does not create duplicities

--- add_abs_fn_path.m
+++ add_abs_fn_path.m
@@ -1,19 +1,11 @@
-function [new_path] = add_abs_fn_path(varargin)
+function [new_path] = inloc_add_abs_fn_path(varargin)
     % addAbsFnPath Adds valid absolute OS filesystem path of
     % specified function name to MATLAB path, if not already present.
     % Relies on the file structure of this repository.
 
     % Get absolute path to folder containing this very file.
     [filepath, ~, ~] = fileparts(mfilename('fullpath'));
-    % OS independent path creation.
-    fn_path = fullfile(filepath, '..', '..', varargin{:});
-    % ispc returns true on Windows which is case insensitive
-    if ~contains( ...
-        [pathsep, path, pathsep], ...
-        [pathsep, fn_path, pathsep], ...
-        'IgnoreCase', ...
-        ispc)
-        addpath(fn_path)
-    end
+    % Addpath does not create duplicities.
+    addpath(fullfile(filepath, '..', varargin{:}));
     new_path = path;
 end
